c74a1c4fd8ecf9255728cdf871379656eb56384e
deekshitha9364 <deekshithagolkonda16@gmail.com>
2026-02-05 11:20:09 +0530

    Create calci.java

---
+++ calci.java
@@ -0,0 +1,16 @@
+class Calculator
+  {
+    public static void main(String args[])
+    {
+      int n1=Integer.parseInt(args[0]);
+      int n2=Integer.parseInt(args[1]);
+      int r1=n1+n2;
+      int r2=n1-n2;
+      int r3=n1*n2;
+      int r4=n1/n2;
+      System.out.println("add:"+r1);
+      System.out.println("sub:"+r2);
+      System.out.println("mul:"+r3);
+      System.out.println("div:"+r4);
+    }
+  }
